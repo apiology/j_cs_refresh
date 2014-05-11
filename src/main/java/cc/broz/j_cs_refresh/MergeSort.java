9e9dd843e1ae7f75e8fb40b58dc64e2b25b8435d
Vince Broz <vince@broz.cc>
2014-05-11 13:34:29 -0400

    Implement MergeSort

--- src/main/java/cc/broz/j_cs_refresh/MergeSort.java
+++ src/main/java/cc/broz/j_cs_refresh/MergeSort.java
@@ -5,36 +5,83 @@ import java.util.ArrayList;
 public class MergeSort implements Sorter
 {
     public <I extends Comparable> ArrayList<I> sort(ArrayList<I> arr) {
-        int size = arr.size();
-        ArrayList<I> ret = new ArrayList<I>(size);
-        if (size == 0) {
-            return ret;
-        }
-        ret.add(arr.get(0));
-        for (int i = 1; i < arr.size(); i++) {
-            I insertableItem = arr.get(i);
-            mergeSortInsert(insertableItem, ret);
-        }
-        return ret;
+        ArrayList<I> result = (ArrayList<I>)arr.clone();
+        mergeSortInPlace(result, 0, arr.size() - 1);
+        return result;
     }
 
-    private <I extends Comparable> void mergeSortInsert(I newCandidateItem,
-                                              ArrayList<I> sortedList) {
-        int indexOfEmptySpace = sortedList.size();
-        sortedList.add(null);
-        for( ; indexOfEmptySpace >= 0; indexOfEmptySpace--) {
-            if (indexOfEmptySpace == 0) {
-                sortedList.set(indexOfEmptySpace, newCandidateItem);
+    private <I extends Comparable> void mergeSortInPlace(ArrayList<I> arr,
+                                                         int startIndex,
+                                                         int endIndex) {
+        int sizeOfSlice = endIndex - startIndex + 1;
+        if (sizeOfSlice <= 1) {
+            // already sorted
+            return;
+        } else {
+            int middleIndex = findMiddleIndex(startIndex, endIndex);
+            mergeSortInPlace(arr, startIndex, middleIndex);
+            mergeSortInPlace(arr, middleIndex+1, endIndex);
+            mergeSlicesInPlace(arr, startIndex, middleIndex, endIndex);
+        }
+    }
+
+    private int findMiddleIndex(int startIndex, int endIndex) {
+        return (startIndex + endIndex)/2;
+    }
+
+    private <I extends Comparable> void mergeSlicesInPlace(ArrayList<I> arr,
+                                                           int startIndex,
+                                                           int middleIndex,
+                                                           int endIndex)  {
+        int sizeOfTotalSlice = endIndex - startIndex + 1;
+
+        int firstIndexOfFirstSegment = startIndex;
+        int currentIndexOfFirstSegment = firstIndexOfFirstSegment;
+        int lastIndexOfFirstSegment = middleIndex;
+
+        int firstIndexOfSecondSegment = middleIndex+1;
+        int currentIndexOfSecondSegment = firstIndexOfSecondSegment;
+        int lastIndexOfSecondSegment = endIndex;
+
+        ArrayList<I> result = new ArrayList<I>(sizeOfTotalSlice);
+
+        while (currentIndexOfFirstSegment <= lastIndexOfFirstSegment &&
+               currentIndexOfSecondSegment <= lastIndexOfSecondSegment) {
+            I firstElement = arr.get(currentIndexOfFirstSegment);
+            I secondElement = arr.get(currentIndexOfSecondSegment);
+            if (firstElement.compareTo(secondElement) < 0) {
+                result.add(firstElement);
+                currentIndexOfFirstSegment++;
             } else {
-                I existingCandidateItem =
-                    sortedList.get(indexOfEmptySpace - 1);
-                if (existingCandidateItem.compareTo(newCandidateItem) > 0) {
-                    sortedList.set(indexOfEmptySpace, existingCandidateItem);
-                } else {
-                    sortedList.set(indexOfEmptySpace, newCandidateItem);
-                    break;
-                }
+                result.add(secondElement);
+                currentIndexOfSecondSegment++;
             }
+        }
+        for (; currentIndexOfFirstSegment <= lastIndexOfFirstSegment;
+             currentIndexOfFirstSegment++) {
+            I firstElement = arr.get(currentIndexOfFirstSegment);
+            result.add(firstElement);
+        }
+        for (; currentIndexOfSecondSegment <= lastIndexOfSecondSegment;
+             currentIndexOfSecondSegment++) {
+            I secondElement = arr.get(currentIndexOfSecondSegment);
+            result.add(secondElement);
+        }
+        copyIntoArray(result, arr, startIndex);
+    }
+
+    private <I extends Comparable> void
+                       copyIntoArray(ArrayList<I> source,
+                                     ArrayList<I> dest,
+                                     int destStartingIndex) {
+        int sourceIndex = 0;
+        int sourceSize = source.size();
+        for (int destIndex = destStartingIndex;
+             destIndex < destStartingIndex + source.size();
+             destIndex++) {
+            assert(sourceIndex < sourceSize);
+            dest.set(destIndex, source.get(sourceIndex));
+            sourceIndex++;
         }
     }
 }
