a0d7e378668b0dee68f799e0c230ce5c19e81914
Vince Broz <vince@broz.cc>
2014-11-16 13:12:43 -0500

    Add

---
+++ src/main/java/cc/broz/j_cs_refresh/ComparableSorter.java
@@ -0,0 +1,9 @@
+package cc.broz.j_cs_refresh;
+
+import java.util.List;
+
+public interface ComparableSorter {
+	// but it can sort any thing of that subtype.
+
+    public <I extends Comparable<I>> void sort(List<I> arr);
+}
