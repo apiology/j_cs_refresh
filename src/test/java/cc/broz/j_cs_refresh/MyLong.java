0f4403b24275690a60401e9ade0b94fd2a2094c7
Vince Broz <vince@broz.cc>
2014-11-16 17:47:19 -0500

    Use BoxedType superclass

--- src/test/java/cc/broz/j_cs_refresh/MyLong.java
+++ src/test/java/cc/broz/j_cs_refresh/MyLong.java
@@ -1,33 +1,7 @@
 package cc.broz.j_cs_refresh;
 
-public class MyLong implements Radixable, Comparable<MyLong> {
+public class MyLong extends BoxedType<Long> implements Radixable {
 	public MyLong(Long l) {
-		this.theLong = l;
-	}
-
-	private Long theLong;
-	
-	public Long getLong() {
-		return theLong;
-	}
-
-	@Override
-	public int compareTo(MyLong other) {
-		return this.theLong.compareTo(other.getLong());
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		return this.theLong.equals(((MyLong)o).getLong());
-	}
-
-	@Override
-	public int hashCode() {
-		return this.theLong.hashCode();
-	}
-
-	@Override
-	public String toString() {
-		return this.theLong.toString();
+		super(l);
 	}
 }
