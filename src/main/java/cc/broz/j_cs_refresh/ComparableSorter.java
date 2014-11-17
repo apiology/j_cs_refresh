1de5867bd57066325d9c10861d25b2892031c95d
Vince Broz <vince@broz.cc>
2014-11-16 21:23:14 -0500

    Kill incorrect comment

--- src/main/java/cc/broz/j_cs_refresh/ComparableSorter.java
+++ src/main/java/cc/broz/j_cs_refresh/ComparableSorter.java
@@ -3,7 +3,5 @@ package cc.broz.j_cs_refresh;
 import java.util.List;
 
 public interface ComparableSorter {
-	// but it can sort any thing of that subtype.
-
-    public <I extends Comparable<? super I>> void sort(List<I> arr);
+	public <I extends Comparable<? super I>> void sort(List<I> arr);
 }
