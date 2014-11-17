1cdef93561c1a5703fee84b4fb6e0db8992da612
Vince Broz <vince@broz.cc>
2014-11-16 21:07:18 -0500

    Fix whitespace

--- src/main/java/cc/broz/j_cs_refresh/RadixSort.java
+++ src/main/java/cc/broz/j_cs_refresh/RadixSort.java
@@ -3,7 +3,7 @@ package cc.broz.j_cs_refresh;
 import java.util.List;
 
 public class RadixSort implements Sorter<Radixable> {
-	public <I extends Radixable> void sort(List<I> arr) {
-		Radixable r = arr.get(0);
-	}
+    public <I extends Radixable> void sort(List<I> arr) {
+        Radixable r = arr.get(0);
+    }
 }
