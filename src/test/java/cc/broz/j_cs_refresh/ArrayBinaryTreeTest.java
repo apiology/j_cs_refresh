fcc7ee5d38861a2dea9b0c31dd16013952252b68
Vince Broz <vince@broz.cc>
2014-12-26 12:40:33 -0500

    Refactor

--- src/test/java/cc/broz/j_cs_refresh/ArrayBinaryTreeTest.java
+++ src/test/java/cc/broz/j_cs_refresh/ArrayBinaryTreeTest.java
@@ -63,62 +63,21 @@ class ArrayBinaryTree<T> {
         set(ROOT_INDEX, root);
     }
 
-    public Cursor getRootCursor() {
+    public Cursor getRoot() {
         return new Cursor(ROOT_INDEX);
     }
 }
 
 public class ArrayBinaryTreeTest extends TestCase {
-    public void testBinaryTreeInitializes() {
-        ArrayBinaryTree<String> abt = new ArrayBinaryTree<>("root");
-    }
-    
-    public void testBinaryTreeRootValueSet() {
-        ArrayBinaryTree<String> abt = new ArrayBinaryTree<>("root");
-        ArrayBinaryTree<String>.Cursor cursor = abt.getRootCursor();
-        assertEquals(cursor.getValue(), "root");
-    }
-
-    public void testBinaryTreeRootLeftValueSet() {
-        ArrayBinaryTree<String> abt = new ArrayBinaryTree<>("root");
-        ArrayBinaryTree<String>.Cursor root = abt.getRootCursor();
-        assertEquals(root.getValue(), "root");
-        ArrayBinaryTree<String>.Cursor rootLeft = root.replaceLeft("left");
-        assertEquals("root", abt.getRootCursor().getValue());
-        assertEquals("left", abt.getRootCursor().getLeft().getValue());
-    }
-
-    public void testBinaryTreeRootRightValueSet() {
-        ArrayBinaryTree<String> abt = new ArrayBinaryTree<>("root");
-        ArrayBinaryTree<String>.Cursor root = abt.getRootCursor();
-        assertEquals(root.getValue(), "root");
-        ArrayBinaryTree<String>.Cursor rootLeft = root.replaceLeft("left");
-        ArrayBinaryTree<String>.Cursor rootRight = root.replaceRight("right");
-        assertEquals("root", abt.getRootCursor().getValue());
-        assertEquals("left", abt.getRootCursor().getLeft().getValue());
-        assertEquals("right", abt.getRootCursor().getRight().getValue());
-    }
-
-
     public void testBinaryTreeRootRightRightValueSet() {
         ArrayBinaryTree<String> abt = new ArrayBinaryTree<>("root");
-        ArrayBinaryTree<String>.Cursor root = abt.getRootCursor();
-        assertEquals(root.getValue(), "root");
-        ArrayBinaryTree<String>.Cursor rootLeft = root.replaceLeft("left");
-        ArrayBinaryTree<String>.Cursor rootRight = root.replaceRight("right");
-        ArrayBinaryTree<String>.Cursor rootRightRight = rootRight.replaceRight("rightright");
-        assertEquals("root", abt.getRootCursor().getValue());
-        assertEquals("left", abt.getRootCursor().getLeft().getValue());
-        assertEquals("right", abt.getRootCursor().getRight().getValue());
-        assertEquals("rightright", abt.getRootCursor().getRight().getRight().getValue());
+        abt.getRoot().replaceLeft("left");
+        abt.getRoot().replaceRight("right").replaceRight("rightright");
+        
+        assertEquals(abt.getRoot().getValue(), "root");
+        assertEquals("root", abt.getRoot().getValue());
+        assertEquals("left", abt.getRoot().getLeft().getValue());
+        assertEquals("right", abt.getRoot().getRight().getValue());
+        assertEquals("rightright", abt.getRoot().getRight().getRight().getValue());
     }
-
-    
-     // void testBinaryTreeAsArray() {
-     //     ArrayBinaryTree<String> abt = new ArrayBinaryTree<>("root");
-     //     Cursor<String> cursor = abt.getRootCursor();
-     //     cursor.addLeft("left");
-     //     cursor.addRight("right");
-         
-     // }
 }
