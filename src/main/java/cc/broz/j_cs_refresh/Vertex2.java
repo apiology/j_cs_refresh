16a7998606781bfe0b439c7e39d80e90e38bdff2
Vince Broz <vince@broz.cc>
2014-12-28 12:53:07 -0500

    Implement BFS

--- src/main/java/cc/broz/j_cs_refresh/Vertex2.java
+++ src/main/java/cc/broz/j_cs_refresh/Vertex2.java
@@ -7,6 +7,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class Vertex2 
     implements IVertex
@@ -22,12 +25,13 @@ public class Vertex2
 
     @Override
     public void addBidirectionalEdge(IVertex other, int distance) {
-        this.neighborsAndDistances.put(other, distance);
+        this.addEdge(other, distance);
+        other.addEdge(this, distance);
     }
 
     @Override
     public void addEdge(IVertex other, int distance) {
-        throw new IllegalStateException("Implement me!");
+        this.neighborsAndDistances.put(other, distance);
     }
 
     @Override
@@ -41,7 +45,51 @@ public class Vertex2
     }
 
     @Override
-    public Map<IVertex,Integer> findDistances() {
+    public Map<IVertex,Integer> findNumberOfHops() {
+        // numberOfHops = {}
+        Map<IVertex,Integer> numberOfHops = new HashMap<>();
+        // color = {} // :grey == enqueued, but not processed.  :black == processed.
+        Set<IVertex> isGrey = new HashSet<>();
+        Set<IVertex> isBlack = new HashSet<>();
+        // parent = {}
+        Map<IVertex,IVertex> parent = new HashMap<>();
+        // q = queue.new
+        Queue<IVertex> q = new ArrayDeque<>();
+        // q.enqueue(this)
+        q.add(this);
+        isGrey.add(this);
+        // numberOfHops[this] = 0
+        numberOfHops.put(this, 0);
+        // parent[this] = nil
+        parent.put(this, null);
+        //
+        // while u = q.dequeue
+        while (!q.isEmpty()) {
+            IVertex u = q.remove();
+        //  if !color[u] == :black
+            if (!isBlack.contains(u)) {
+        //      for v : q.siblings
+                for (IVertex v : u.getNeighbors()) {
+        //          if color[v] == :white
+                    if (!isGrey.contains(v) && !isBlack.contains(v)) {
+        //              parent[v] = u
+                        parent.put(v, u);
+        //              numberOfHops[v] = numberOfHops[u] + 1
+                        numberOfHops.put(v, numberOfHops.get(u) + 1);
+        //              color[v] = :grey
+                        isGrey.add(v);
+        //              q.eneuque(v)
+                        q.add(v);
+                    }
+                }
+            }
+            isBlack.add(u);
+        //   color[u] = :black
+        }
+        return numberOfHops;
+    }
+
+    public Map<IVertex,Integer> findDistancesBlah() {
         Map<IVertex, Integer> distance = new HashMap<>();
         distance.put(this, 0);
         BFS bfs = new BFS();
